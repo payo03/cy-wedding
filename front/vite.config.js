9c5894927d84d17207654f730c6231d6e40e2076
송찬영 <52197226+payo03@users.noreply.github.com>
2025-07-28 13:35:51 +0900

    07-28 Render 설정파일 재반영

--- front/vite.config.js
+++ front/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     host: true,
     proxy: {
-      '/api': 'http://localhost:10000',
+      '/api': 'http://localhost:8080',
     },
     historyApiFallback: true,
   },
